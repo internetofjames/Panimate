e0e2e325e257cd76321666751ea950fe9315f838
james-bertrand <james.bertrand89@gmail.com>
2019-03-26 13:07:33 -0500

    Getting repository up to date

---
+++ Source/Panner.h
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    Panner.h
+    Created: 28 Feb 2019 1:43:53pm
+    Author:  James Bertrand
+
+  ==============================================================================
+*/
+
+#ifndef PANNER_H
+#define PANNER_H
+
+#include <stdio.h>
+
+class Panner {
+    
+    
+    
+    
+};
+
+#endif /* Panner.h */
