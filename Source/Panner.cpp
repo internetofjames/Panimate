bf2889bf532de84a02ebbc093b1e44fba2670d2a
james-bertrand <james.bertrand89@gmail.com>
2019-04-15 19:59:43 -0500

    finished panner class (mostly)...

--- Source/Panner.cpp
+++ Source/Panner.cpp
@@ -9,6 +9,23 @@
 */
 
 #include "Panner.h"
+#include <math.h>
+
+float Panner::processSample(float x,int channel){
+    
+    this->channel = channel;
+    
+    // Output variable
+    float y;
+    float lfo;
+    
+    lfo = getNextSample();
+    
+    y = x * lfo;
+    
+    return y;
+    
+};
 
 // Set the sample rate that the panner should operate at
 // This should be set by your DAW
@@ -26,6 +43,8 @@ int Panner::getFs(){
 // Set the period of the LFO in milliseconds
 void Panner::setRate(float rateInMilliseconds){
     rate = rateInMilliseconds;
+    
+    angleChange = ((1.0f/rateInMilliseconds) * 2.0f * M_PI / (float)Fs) + phaseOffset;
 }
 
 float Panner::getRate(){
@@ -83,3 +102,106 @@ void Panner::setLFOType(TypeLFO type){
 Panner::TypeLFO Panner::getLFOType(){
     return LFO;
 };
+
+// LFO functions
+float Panner::sineFunction(){
+    float out = sinf(currentAngle[channel]);
+    
+    updateAngle();
+    
+    return out;
+}
+
+float Panner::squareFunction(){
+    float out;
+    if(currentAngle[channel] < M_PI){
+        out = 1.0f;
+    }
+    else {
+        out = -1.0f;
+    }
+    
+    updateAngle();
+    
+    return out;
+}
+
+
+float Panner::sawtoothFunction(){
+    float out = 2.0f * (currentAngle[channel]/(2.0f*M_PI)) - 1.0f;
+    
+    updateAngle();
+    
+    return out;
+}
+
+
+float Panner::triangleFunction(){
+    float out;
+    if(currentAngle[channel] < M_PI){
+        out = 2.0f * (currentAngle[channel]/(M_PI)) - 1.0f;
+    }
+    else {
+        float temp = 1.0f - ((currentAngle[channel]-M_PI)/(M_PI));
+        out = 2.0f * temp - 1.0f;
+    }
+    
+    updateAngle();
+    
+    return out;
+}
+
+
+
+void Panner::updateAngle(){
+    currentAngle[channel] += angleChange;
+    if (currentAngle[channel] > 2*M_PI){
+        currentAngle[channel] -= (2*M_PI);
+    }
+}
+
+
+float Panner::getNextSample(){
+    
+    float lfo=0.0f;
+    
+    switch (LFO) {
+        case sine:
+            lfo = depth * sineFunction() + positionOffset;
+            break;
+            
+        case square:
+            lfo = depth * squareFunction() + positionOffset;
+            break;
+            
+        case sawtooth:
+            lfo = depth * sawtoothFunction() + positionOffset;
+            break;
+            
+        case triangle:
+            lfo = depth * triangleFunction() + positionOffset;
+            break;
+            
+        default: // sine as a back-up
+            lfo = depth * sineFunction() + positionOffset;
+            break;
+    }
+    
+    // if the combination of user defined parameters causes the LFO to exceed the maximum
+    // possible pan values of -100 (full left) or 100 (full right) set them to the max
+    if (lfo > 100){
+        lfo = 100.0f;
+    }
+    if (lfo < -100){
+        lfo = -100.0f;
+    }
+    
+    int phaseInvert = 1;
+    if (phaseInverted) {
+        phaseInvert = -1;
+    }
+    
+    lfo *= phaseInvert;
+    
+    return lfo;
+}
